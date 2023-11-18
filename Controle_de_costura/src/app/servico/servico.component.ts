8f34e181230419dab20128a59226401e7c722a0a
Elias Junior <eliasjunior1405@gmail.com>
2023-11-18 10:56:45 -0300

    captura de serviços

--- Controle_de_costura/src/app/servico/servico.component.ts
+++ Controle_de_costura/src/app/servico/servico.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CadServico } from '../models/CadServico';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ServicesService } from '../services/services.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-servico',
@@ -13,67 +15,25 @@ export class ServicoComponent implements OnInit{
   public servicoSelecionado: CadServico;
   public mostrarBotaoEditar: boolean = false;
 
+ //servicos:CadServico[] = []
+ servicos$ = new Observable<CadServico[]>();
+  
 
-  public servicos= [
-    {id:1, nome: 'Jô', descricao: 'feito uma blusa e uma jaqueta'     ,valor:20, data:this.formatarData(new Date()),numero:'99949512'},
-    {id:2, nome: 'Talita', descricao: 'feito uma blusa e uma jaqueta' ,valor:20, data:this.formatarData(new Date()),numero:'99949513'},
-    {id:3, nome: 'Nathi', descricao: 'feito uma blusa e uma jaqueta'  ,valor:20, data:this.formatarData(new Date()),numero:'99949514'},
-    {id:4, nome: 'Hericles', descricao:'feito uma blusa e uma jaqueta',valor:20, data:this.formatarData(new Date()),numero:'99949515'},
-    {id:5, nome: 'Heraldo', descricao: 'feito uma blusa e uma jaqueta',valor:20, data:this.formatarData(new Date()),numero:'99949516'},
-  ];
-
-  constructor( private fb: FormBuilder){
-    this.criarForm();
+  constructor( private services :ServicesService ){
+    this.obterServicos();
   }
 
   ngOnInit(): void {
     
   }
 
-  criarForm(){
-    this.servicoForm = this.fb.group({
-      nome:['', Validators.required],
-      valor: ['',Validators.required],
-      numero:['',Validators.required],
-      descricao:['',Validators.required,]
-  });
+  obterServicos(){
+    this.servicos$ = this.services.getServicos();
   }
-  servicoEdit(){
-    //this.servicoForm.value;
-    this.servicoForm.patchValue(CadServico);
-    
-    
-  }
-  Edit(servico: CadServico) {
-    this.servicoSelecionado = servico;
-    this.servicoForm.patchValue(servico);
-    this.mostrarBotaoEditar = false; 
-    
-  }
-  
 
-  servicoSelect(servico:CadServico){
-    this.servicoSelecionado = servico;
-    this.servicoForm.patchValue(servico);
-    this.mostrarBotaoEditar = true;
-  }
+
   voltar(){
     this.servicoSelecionado = null;
-  }
-  excluir(servico: CadServico): void {
-    const index = this.servicos.findIndex(s => s.id === servico.id);
-    if (index !== -1) {
-      this.servicos.splice(index, 1);
-    }
-    
-    if (this.servicoSelecionado && this.servicoSelecionado.id === servico.id) {
-      this.servicoSelecionado = null;
-    }
-  }
-  
-
-  formatarData(data: Date): string {
-    return data.toLocaleDateString('pt-BR'); 
   }
 
 }
